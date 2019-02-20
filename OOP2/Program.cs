d489889ecb631c734be6f602aa97bb3089a66dea
maevemoontea <37961108+maevemoontea@users.noreply.github.com>
2019-02-20 16:26:31 +0200

    First commit. Zero functionality.

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -1,15 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace OOP2
 {
     class Program
     {
+        static string baseDirPath = "";
+        static string resultDirPath = "";
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            SayHello();
+            TakeData();
+            SayGoodbay();
+        }
+        static void SayHello()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("   *              ООП-2              *                  ");
+            Console.WriteLine("   *        Лабораторна Робота       *                  ");
+            Console.WriteLine("   *        Сичевської Альони        *                  ");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n Програма виконує обхід заданого каталогу у пошуках файлів зображень та створює копію структури каталогів із розміщенням замість файлів зображень файлів -- результатів обробки зображень. \n\n");
+
+            Console.BackgroundColor = default;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        static void TakeData()
+        {
+            string path = "";
+            Console.WriteLine("Set path for directory where you need looking for. For example \n D:\\some_folder \n");
+            path = Console.ReadLine();
+            baseDirPath = SetDirectory(path);
+
+            Console.WriteLine("Set path for directory to place results \n");
+            path = Console.ReadLine();
+            resultDirPath = SetDirectory(path);
+        }
+        static string SetDirectory(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (dir.Exists)
+            {
+                return path;
+            }
+            else
+            {
+                Console.WriteLine("Set another path. This directory doesn't exist!");
+                path = Console.ReadLine();
+                return SetDirectory(path);
+            }
+        }
+        static void SayGoodbay()
+        {
+            Console.WriteLine("\n Good luck! \n\n Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ OOP3Tests/ProgramTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+using OOP2;
+
+namespace OOP3Tests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        
+    }
+}
